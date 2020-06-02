f3928906bef0333c70c1be9ba712600d82a7d4dc
SanjaiKumar80 <62129419+SanjaiKumar80@users.noreply.github.com>
2020-06-02 09:55:50 +0530

    Initial commit

---
+++ app/src/main/java/com/training/viewmodel/MainActivity.java
@@ -0,0 +1,36 @@
+package com.training.viewmodel;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    HeroesAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        HeroesViewModel model = ViewModelProviders.of(this).get(HeroesViewModel.class);
+        model.getHeroes().observe(this, new Observer<List<Hero>>() {
+            @Override
+            public void onChanged(@Nullable List<Hero> heroList) {
+                adapter = new HeroesAdapter(MainActivity.this, (ArrayList<Hero>) heroList);
+                recyclerView.setAdapter(adapter);
+            }
+        });
+
+    }
+}
